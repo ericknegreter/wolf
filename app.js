cbed3f2e81e4ff00577bb0219668a16606746902
erick <erickxon-roy@hotmail.com>
2017-10-10 14:07:41 -0500

    Se modifico el puerto al que se accede

--- app.js
+++ app.js
@@ -5,13 +5,15 @@
 
 //console.log(nombre, 'es', nick);
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!');
+});
